71ef6db03e064059f8e2a0acf1dac1da1955c518
p.rabau <p.rabau@godeltech.com>
2020-02-21 14:23:03 +0300

    Initial Commit

---
+++ trivial-architecture-core/TrivialArchitecture.UI.Console/Infrastructure/IoC/Modules/DbContextModule.cs
@@ -0,0 +1,31 @@
+﻿using Autofac;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using TrivialArchitecture.DAL;
+
+namespace TrivialArchitecture.UI.Console.Infrastructure.IoC.Modules
+{
+	public class DbContextModule : Module
+	{
+		protected override void Load(ContainerBuilder builder)
+		{
+			base.Load(builder);
+
+			ConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
+			configurationBuilder.Sources.Clear();
+			configurationBuilder.AddJsonFile("appsettings.json", optional: false, reloadOnChange: false);
+			IConfigurationRoot configurationRoot = configurationBuilder.Build();
+
+			string connectionString = configurationRoot.GetConnectionString("TrivialArchitectureDB");
+
+			var dbContextOptionsBuilder = new DbContextOptionsBuilder<TrivialArchitectureDbContext>()
+				.UseSqlServer(connectionString);
+		
+			builder
+				.RegisterType<TrivialArchitectureDbContext>()
+				.WithParameter("options", dbContextOptionsBuilder.Options)
+				.As<DbContext>()
+				.InstancePerLifetimeScope();
+		}
+	}
+}
